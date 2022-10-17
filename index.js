57f1cc86b0331c6dd5a831f71274327770b66c5e
JiiXaa <anythingfx@gmail.com>
2022-10-17 19:15:19 +0100

    Mobile menu toggle bahaviour, toggle on scroll and when overlay clicked

--- index.js
+++ index.js
@@ -9,10 +9,24 @@ console.log(menuBtn);
 console.log(overlay);
 console.log(navigationMainList);
 
-// Mobile Menu Logic
+//////////////////////
+/// Mobile Menu Logic
 
+// Menu toggle on hamburger menu click
 menuBtn.addEventListener('click', () => {
   showMenu();
+});
+
+// Menu close when overlay clicked
+overlay.addEventListener('click', () => {
+  showMenu();
+});
+
+// Menu close on scroll event
+window.addEventListener('scroll', () => {
+  if (navigationMainList.classList.contains('show')) {
+    showMenu();
+  }
 });
 
 function showMenu() {
